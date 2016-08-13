a4ffd125439c4deb9d48014227f7dfba9b75ff47
Vishwajeet Narwal <vnarwal95@gmail.com>
2016-08-13 02:20:24 -0400

    image and article count of stories(backend+frontend), responsive image in images.html

--- crowdsourcing/serializers/garner.py
+++ crowdsourcing/serializers/garner.py
@@ -6,12 +6,26 @@ class SubscriptionSerializer(serializers.ModelSerializer):
         model = Subscription
 
 class StorySerializer(serializers.ModelSerializer):
+    number_of_articles = serializers.SerializerMethodField()
+    number_of_images = serializers.SerializerMethodField()
     class Meta:
         model = Story
+
+    def get_number_of_articles(self, obj):
+        return obj.article_set.count()
+
+    def get_number_of_images(self, obj):
+        articles = obj.article_set.all()
+        image_count=0
+        for article in articles:
+            image_count = image_count + article.image_set.count()
+        return image_count
+
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
+
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
