3fe321ebbe6433e2be5558c99c37a0ea0934d3c0
Vishwajeet Narwal <vnarwal95@gmail.com>
2016-12-24 22:17:29 -0600

    Dragging of images : done

--- static/js/visual/directives/collage.directive.js
+++ static/js/visual/directives/collage.directive.js
@@ -24,6 +24,9 @@
         element[0].width = stageBounds.width;
         element[0].height = stageBounds.height;
         var gridImg, gridBitmap = null; //perspective plane
+        var gridBounds;
+        var editItem; //reference to the bitmap being manipulated
+
         initStage();
 
         function initStage(){
@@ -35,15 +38,18 @@
           } else {
             scope.stage = new createjs.Stage(element[0]);
           }
+          stageBounds.midX = stageBounds.width/2;
+          stageBounds.midY = stageBounds.height/2;
+
 
 
           if (createjs.Touch.isSupported()) {
             createjs.Touch.enable(stage);
           }
 
-          loadGrid();
-          loadImage_one();
-          loadImage_two();
+          // loadGrid();
+          loadImage(scope.images[0].file);
+          loadImage(scope.images[1].file);
         }
 
         function loadGrid(){
@@ -55,31 +61,87 @@
         function drawGrid(event){
           gridBitmap = new createjs.Bitmap(event.target);
           scope.stage.addChild(gridBitmap);
+          gridBounds = gridBitmap.getBounds();
+          gridBounds.y = stageBounds.height - gridBounds.height;
+          gridBitmap.x = 0;
+          gridBitmap.y = gridBounds.y;
           scope.stage.update();
         }
 
-        function loadImage_one(){
-          gridImg = new Image();
-          gridImg.src = scope.images[0].file;
-          gridImg.onload = drawImage_one;
+        function loadImage(image_file){
+          var im = new Image();
+          im.src = image_file;
+          im.onload = drawImage;
         }
 
-        function drawImage_one(event){
-          gridBitmap = new createjs.Bitmap(event.target);
-          scope.stage.addChild(gridBitmap);
+        function drawImage(event){
+          var image = new createjs.Bitmap(event.target);
+          scope.stage.addChild(image);
+          centerElement(image);
+          addBitmapListeners(image);
           scope.stage.update();
         }
 
-        function loadImage_two(){
-          gridImg = new Image();
-          gridImg.src = scope.images[1].file;
-          gridImg.onload = drawImage_two;
+        function centerElement(el){
+          el.regX = el.image.width >> 1;
+          el.regY = el.image.height >> 1;
+          el.x = stageBounds.width >> 1;
+          el.y = stageBounds.height >> 1;
         }
 
-        function drawImage_two(event){
-          gridBitmap = new createjs.Bitmap(event.target);
-          scope.stage.addChild(gridBitmap);
+        function addBitmapListeners(bitmap){
+          bitmap.on("mousedown", onBitmapDown);
+          bitmap.on('click',toggleBitmapManipulation);
+          bitmap.on("pressmove",onDragBitmap);
+          bitmap.on('pressup',onDragUp);
+        }
+
+        var onBitmapDown = function(e){
+          //this.parent.addChild(this); //MH - will move to front of z order
+          this.offset = {x: this.x - e.stageX, y: this.y - e.stageY};
+        }
+        var onDragBitmap = function(e){
+          if (this.editable){
+            this.dragging = true;
+            this.x = e.stageX + this.offset.x;
+            this.y = e.stageY + this.offset.y;
+            scope.stage.update();
+          }
+
+        }
+        var onDragUp = function(){
+          this.dragging = false;
+        }
+
+        var toggleBitmapManipulation = function(e){
+          if (editItem && editItem.id != this.id){ //we are switching to a new item
+            editItem.editable = false;
+            removeGlow(editItem);
+            editItem = this.id;
+            // adjustSliderValues();
+          }
+
+          if (this.editable){ //item is editable if it has been selected
+            if (!this.dragging){
+              this.editable = false;
+              removeGlow(this);
+              editItem = null;
+            }
+          } else {
+            editItem = this;
+            this.editable = true;
+            addGlow(this);
+          }
           scope.stage.update();
+
+        }
+
+        var addGlow = function(bitmap){
+          bitmap.shadow = new createjs.Shadow("#ff0000", 0, 0, 5);
+        }
+
+        var removeGlow = function(bitmap){
+          bitmap.shadow = null;
         }
 
 
