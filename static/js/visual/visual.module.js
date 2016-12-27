d62ed7af9f6fafd377f7a42956f4f0f1c23f3049
Vishwajeet Narwal <vnarwal95@gmail.com>
2016-12-26 23:55:45 -0600

    adding dynamic metatags

--- static/js/visual/visual.module.js
+++ static/js/visual/visual.module.js
@@ -5,15 +5,18 @@
     .module('crowdsource.visual', [
       'crowdsource.visual.controllers',
       'crowdsource.visual.services',
-      'crowdsource.visual.directives'
+      'crowdsource.visual.directives',
+      'updateMeta'
+
+
     ]);
 
   angular
-    .module('crowdsource.visual.controllers', []);
+    .module('crowdsource.visual.controllers', ['updateMeta']);
 
   angular
     .module('crowdsource.visual.services', ['ngCookies']);
 
   angular
-    .module('crowdsource.visual.directives', []);
+    .module('crowdsource.visual.directives', ['updateMeta']);
 })();
